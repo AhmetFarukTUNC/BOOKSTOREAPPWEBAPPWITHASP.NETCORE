92b973986daeedc8ceaa5a68efa23d615669b425
AhmetFarukTUNC <atunc8402@gmail.com>
2024-08-27 09:42:49 +0300

    We have been rate limiting implementation.

--- bsStoreApp/Presentation/obj/Debug/net6.0/Presentation.AssemblyInfo.cs
+++ bsStoreApp/Presentation/obj/Debug/net6.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+628a0d498a240b6d2a9f4f30d1e8f34740203061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38aa50fe05b3a50055515f223bdff16cb66c2b09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- bsStoreApp/Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ bsStoreApp/Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+628a0d498a240b6d2a9f4f30d1e8f34740203061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38aa50fe05b3a50055515f223bdff16cb66c2b09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
